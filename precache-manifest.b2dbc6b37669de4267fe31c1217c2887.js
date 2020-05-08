1c27b871156807e71bf24ac8c4acb4ea58d2af60
Ravi Prakash Singh <Ravip@cloudthing.com>
2020-05-08 13:27:41 +0530

    Updates

---
+++ precache-manifest.b2dbc6b37669de4267fe31c1217c2887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9acd64a3c23d61a1daf949f0f02fbe1",
+    "url": "/ems-login/index.html"
+  },
+  {
+    "revision": "1049eb96ae1f07844a88",
+    "url": "/ems-login/static/css/main.0c207b18.chunk.css"
+  },
+  {
+    "revision": "c767400052dbe8e106b5",
+    "url": "/ems-login/static/js/2.ec8c1974.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ems-login/static/js/2.ec8c1974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1049eb96ae1f07844a88",
+    "url": "/ems-login/static/js/main.e71c7729.chunk.js"
+  },
+  {
+    "revision": "caedb67526069ba06e84",
+    "url": "/ems-login/static/js/runtime-main.ad05ff17.js"
+  },
+  {
+    "revision": "50ae0299af47d101eb6dd02041ebcc35",
+    "url": "/ems-login/static/media/company.50ae0299.jpg"
+  }
+]);
